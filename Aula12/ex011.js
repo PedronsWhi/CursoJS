1b06b88c8ea4e8c9f7e09735043a0a6b08cca355
PedronsWhi <pedrocs12340@gmail.com>
2024-04-17 22:47:57 -0300

    Atualizações Dia: 17 de Abril

---
+++ Aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 100
+if(idade < 16){
+    console.log(`Não pode votar!`)
+}else if(idade < 18){
+    console.log(`Voto Opcional!`)
+}else if(idade >= 18){
+    console.log(`Voto Obrigatorio!`)
+}else if(idade >= 67){
+    console.log(`Voto opcional!`)
+}
