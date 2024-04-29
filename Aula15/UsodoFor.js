57d6074e8445468fd73e8963e8a899a145396999
PedronsWhi <pedrocs12340@gmail.com>
2024-04-29 17:12:46 -0300

    Atualização

---
+++ Aula15/UsodoFor.js
@@ -0,0 +1,4 @@
+num = [4,5,6,7,8]
+for(let pos = 0; pos , num.length; pos++){
+    console.log(num[pos])
+}
---
+++ Aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [9,2,3,6,7]//Mais de um valor para uma variável 
+
+num[6] = 8 //A posição 8 recebe 5
+num.push(4)//Acrescenta mais um valor dentro da variável
+num.sort()//organiza do menor para o maior
+
+console.log(num)//printa o valor da varialvel
+console.log(`O vetor tem ${num.length} posições`)//num.length mostra quantas posições existem na variável "num"
+console.log(`O primeiro valor da variável é ${num[0]}`)//mostra quais valores estão implementados nas posições
